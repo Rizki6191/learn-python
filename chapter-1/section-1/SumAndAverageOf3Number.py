7adac5f5e55d82f97e51afb8a290baa9433e4785
Rizki6191 <143875649+Rizki6191@users.noreply.github.com>
2025-04-17 11:11:57 +0700

    Create SumAndAverageOf3Number.py

---
+++ chapter-1/section-1/SumAndAverageOf3Number.py
@@ -0,0 +1,11 @@
+sum = 0
+average = 0
+
+sum1=float(input("Enter number 1:"))
+sum2=float(input("Enter number 2:"))
+sum3=float(input("Enter number 3:"))
+
+sum=sum1+sum2+sum3
+average=sum/3.0
+print("Sum =",sum)
+print("Average =",average)
