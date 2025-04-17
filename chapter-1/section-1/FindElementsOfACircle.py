a56f139dbd2a9e183baa870fb1086ca157180cac
Rizuki <143875649+Rizki6191@users.noreply.github.com>
2025-04-17 12:34:14 +0700

    Create FindElementsOfACircle.py

---
+++ chapter-1/section-1/FindElementsOfACircle.py
@@ -0,0 +1,9 @@
+PI = 3.14
+radius = float(input("Enter the radius of a cicle: "))
+diameter = 2 * radius
+circumference = 2 * PI * radius
+area = PI * radius**2
+
+print("Diameter of a circle = %.2f"%diameter)
+print("Circumference of a cicle = %.2f"%circumference)
+print("Area of a cicle = %.2f"%area)
