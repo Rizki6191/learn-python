075523d5b9ac447b0c551227d5bd8f56b2650156
Rizuki <143875649+Rizki6191@users.noreply.github.com>
2025-04-19 12:57:54 +0700

    Create function1.py

---
+++ chapter-3/function1.py
@@ -0,0 +1,9 @@
+def sum(n1,n2):
+    add = n1 + n2
+    return add
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = sum(num1,num2)
+print("\nThe sum is: ", result)
